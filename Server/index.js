9aa65c085918a6a65fe847eddcb09a5773786f9e
Fedottt-Bo <77093554+Fedottt-Bo@users.noreply.github.com>
2021-06-14 15:16:49 +0300

    WIP

--- Server/index.js
+++ Server/index.js
@@ -1,31 +1,47 @@
 const http = require("http");
+const url = require('url');
 const fs = require('fs').promises;
 
 const host = 'localhost';
 const port = 8000;
 
+var messages = [];
+
 const requestListener = function (req, res) {
     console.log(
         `Request: ${req.method}, ${req.url}`
-    );
-    console.log(
-        `Request headers: ${JSON.stringify(req.headers)}`
     );
 
     let fileName;
     let contentType;
 
-    if (req.url === "/") {
-        fileName = "index.html";
-        contentType = "text/html";
+    if (req.method === 'POST') {
+        const data = url.parse(req.url, true).query;
+        console.log(data);
+        try {
+            messages.push({ name: data.name, time: data.time, text: data.text });
+        }
+        catch (err) { }
     }
-    else if (req.url.endsWith(".css")) {
-        fileName = req.url.substr(1);
-        contentType = "text/css";
-    } else {
-        res.writeHead(500);
-        res.end("Error, unsupported");
-        return;
+    else if (req.method === 'GET') {
+        if (req.url === "/") {
+            fileName = "index.html";
+            contentType = "text/html";
+        }
+        else if (req.url === "/Update") {
+            res.setHeader("Content-Type", "text/html");
+            res.writeHead(200);
+            res.end(GenMessageList());
+            return;
+        }
+        else if (req.url.endsWith(".css")) {
+            fileName = req.url.substr(1);
+            contentType = "text/css";
+        } else {
+            res.writeHead(500);
+            res.end("Error, unsupported");
+            return;
+        }
     }
 
     fs.readFile(`${__dirname}/${fileName}`)
@@ -41,7 +57,14 @@ const requestListener = function (req, res) {
         });
 };
 
+function GenMessageList() {
+    tmp = "";
+    messages.forEach(function (val, i) { tmp += `${val.name} (${val.time}): ${val.text}\n`; });
+    return tmp;
+}
+
 const server = http.createServer(requestListener);
 server.listen(port, host, () => {
     console.log(`Server is running on http://${host}:${port}`);
 });
+console.log(messages);
